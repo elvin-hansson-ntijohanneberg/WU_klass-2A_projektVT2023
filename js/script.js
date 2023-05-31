be15ee9d57148c299afe269d394e8d5edd5493db
Elvin Hansson <116065542+elvin-hansson-ntijohanneberg@users.noreply.github.com>
2023-05-31 13:51:53 +0200

    vecka 21

--- js/script.js
+++ js/script.js
@@ -7,3 +7,38 @@ function toggleMenu() {
 }
 
 burger.addEventListener('click', toggleMenu)
+
+
+/* const blinkBox = document.getElementsById('idiv_1');
+
+blinkBox.addEventListener('mouseover', () => {
+    alert('Hej');
+    const blinkBorder = setInterval (() => { 
+        blinkBox.style.borderColor = blinkBox.style.borderColor === 'transparent' ? '#ccc' : 'transparent';
+    }, 400);
+
+    blinkBox.addEventListener('mouseout', () => {
+        clearInterval(blinkBorder);
+        blinkBox.style.borderColor = '#ccc';
+
+    }, {once: true});
+});
+*/
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
